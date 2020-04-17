d541533786136cc391636dd0587a211ef3687537
Andy <a.ortizg@uniandes.edu.co>
2020-04-17 18:22:49 -0500

    Add neighorhood init sql

---
+++ src/assets/neighborhood.sql
@@ -0,0 +1,17 @@
+delete from NeighborhoodEntity;
+
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (100, 'Ciudad Salitre', 'Engativá', 100);
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (200, 'Chicó Norte', 'Chicó', 800);
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (300, 'Kennedy', 'Bosa', 1000);
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (400, 'Santa Barbara', 'Usaquén', 300);
+
+
+delete from NeighborhoodEntity;
+
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (100, 'Ciudad Salitre', 'Engativá', 100);
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (200, 'Chicó Norte', 'Chicó', 800);
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (300, 'Kennedy', 'Bosa', 1000);
+insert into NeighborhoodEntity (id, name, locality, numberOfResidents) values (400, 'Santa Barbara', 'Usaquén', 300);
+
+
+
