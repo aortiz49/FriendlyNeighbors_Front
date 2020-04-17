be480677905ffe28dee9b3b89b92c76c20660d61
Andy <a.ortizg@uniandes.edu.co>
2020-04-17 16:40:59 -0500

    Black slate

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+const host = 'localhost';
+const port = '8080';
+const appName = 's3_neighborhood-api';
+const rootApi = 'api';
+
+export const environment = {
+  production: false,
+  apiUrl: `http://${host}:${port}/${appName}/${rootApi}/`
+};
