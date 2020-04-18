5ff69cafbd82b4b29e45b871f9a2519a908269a9
Andres Bayona <al.bayona@uniandes.edu.co>
2020-04-18 00:08:50 -0500

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PostModule} from "./post/post.module";
+import {ResidentModule} from "./resident/resident.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PostModule,
+    ResidentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
