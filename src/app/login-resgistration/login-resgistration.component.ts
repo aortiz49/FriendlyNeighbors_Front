4b5c4fbe397a9472c2ea1859301e86ef3ece756b
Andy <a.ortizg@uniandes.edu.co>
2020-04-26 19:29:21 -0500

    Add thumbprint

--- src/app/login-resgistration/login-resgistration.component.ts
+++ src/app/login-resgistration/login-resgistration.component.ts
@@ -5,17 +5,16 @@ import { Neighborhood } from '../neighborhood/neighborhood';
 @Component({
   selector: 'app-login-resgistration',
   templateUrl: './login-resgistration.component.html',
-  styleUrls: ['./login-resgistration.component.css']
+  styleUrls: ['./login-resgistration.component.css'],
 })
 export class LoginResgistrationComponent implements OnInit {
-
-  constructor(private neighborhoodService: NeighborhoodService) { }
+  constructor(private neighborhoodService: NeighborhoodService) {}
 
   neighborhoods: Array<Neighborhood>;
 
   getNeighborhoods(): void {
     this.neighborhoodService.getNeighborhoods().subscribe((neighborhoods) => {
-     // this.neighborhoods = neighborhoods.slice(0,4);
+      // this.neighborhoods = neighborhoods.slice(0,4);
       this.neighborhoods = neighborhoods;
     });
   }
@@ -23,5 +22,4 @@ export class LoginResgistrationComponent implements OnInit {
   ngOnInit() {
     this.getNeighborhoods();
   }
-
 }
