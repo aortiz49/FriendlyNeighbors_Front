c71469c8e45d329311b7419d5a3b7e78e8ad0dcc
Andres Bayona <al.bayona@uniandes.edu.co>
2020-04-30 14:14:27 -0500

    CHanges

---
+++ src/app/home/resident-detail.ts
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import {Resident} from '../home/resident';
+import {Post} from "../post/post";
+import {CommentP} from "../post/commentP";
+import {Favor} from "../favor/favor";
+
+/**
+ *This class represents a resident made by a resident
+ */
+export class ResidentDetail extends Resident {
+
+
+  /**
+   * Posts shared with this resident.
+   */
+  public postsToView: Post[];
+
+
+  /**
+   * Represents posts made by this resident
+   */
+  public posts: Post[];
+
+  public favorsRequested: Favor[];
+
+  /**
+   * Represents comments posted by this resident
+   */
+  public comments: CommentP[];
+
+
+}
+
+
---
+++ src/app/home/resident.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
+import {ResidentDetail} from "./resident-detail";
+
+const API_URL = environment.apiUrl;
+const residents = "/residents";
+const neighborhoods = "/neighborhoods";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResidentService {
+
+  constructor(private http:HttpClient) { }
+
+  getresidents(neighborhood):Observable<ResidentDetail[]>{
+    return this.http.get<ResidentDetail[]>(
+      API_URL + neighborhoods + "/" + neighborhood + residents + "/"
+    );
+  }
+
+  getresident(neighborhood, resident):Observable<ResidentDetail>{
+    return this.http.get<ResidentDetail>(
+      API_URL + neighborhoods + "/" + neighborhood + residents + "/" + resident
+    );
+  }
+
+  getposts(neighborhood, resident):Observable<ResidentDetail>{
+    return this.http.get<ResidentDetail>(
+      API_URL + neighborhoods + "/" + neighborhood + residents + "/" + resident
+    );
+  }
+
+  getfavors(neighborhood, resident):Observable<ResidentDetail>{
+    return this.http.get<ResidentDetail>(
+      API_URL + neighborhoods + "/" + neighborhood + residents + "/" + resident
+    );
+  }
+
+
+
+
+}
+
+
