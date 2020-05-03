ef2cf344ac4ca7e24725b6422477c8435f2d65c9
Andy <a.ortizg@uniandes.edu.co>
2020-05-02 23:24:49 -0500

    Add favors in neighborhood menu

--- src/app/business/business-routing/business-routing.module.ts
+++ src/app/business/business-routing/business-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BusinessListComponent } from '../business-list/business-list.component';
 import { MainLayoutComponent } from 'src/app/neighborhoodLayout/main-layout/main-layout.component';
+import { BusinessDetailComponent } from '../business-detail/business-detail.component';
 
 const routes: Routes = [
   {
     path: 'neighborhoods/:id/businesses',
     component: MainLayoutComponent,
     children: [ { path: '', component: BusinessListComponent } ]
+  },
+  {
+    path: 'neighborhoods/:id/businesses/:lid',
+    component: MainLayoutComponent,
+    children: [ { path: '', component: BusinessDetailComponent } ]
   }
 ];
 
